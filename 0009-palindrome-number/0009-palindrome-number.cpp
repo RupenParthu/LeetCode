ce32a8f9946de6d402156f9e89645a638fb06a9d
RupenParthu <rupenparthu@gmail.com>
2025-05-05 12:25:13 +0530

    Time: 0 ms (100%), Space: 8.6 MB (69.12%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        unsigned long long rem=0, rev=0;
-        unsigned long long temp = x;
+        long long rem=0, rev=0;
+        long long temp = x;
         if(x<0){
             return false;
         }
+        
         while(x!=0){
             rem = x%10;
             rev = rev*10 + rem;
