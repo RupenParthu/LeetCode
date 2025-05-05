8386e302af20aa40eaf125a7ca5a12802747ff61
RupenParthu <rupenparthu@gmail.com>
2025-05-05 23:07:49 +0530

    Time: 0 ms (100%), Space: 7.9 MB (22.55%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -5,10 +5,10 @@ public:
         if(num<=1) return false;
         for(int i=2;i*i<num;i++){
             if(num%i==0){
-                sum+=i;
-                if(i != num / i){
-                    sum += num / i;
-                }
+                 sum+=i;
+                 if(i != num / i){
+                     sum += num / i;
+                 }
             }
         }
         if(sum==num){
