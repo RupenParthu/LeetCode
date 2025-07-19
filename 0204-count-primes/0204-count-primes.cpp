3c885aa360bb0b6fccf674f7b138b2b1ab30f9c0
RupenParthu <rupenparthu@gmail.com>
2025-07-19 13:54:56 +0530

    Time: 235 ms (24.84%), Space: 12.6 MB (91.52%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) {
-       int cnt = 0;
-       //using seivemalgo for prime counting;
-       vector<bool>seive(n,true);
-       if(n==0 || n==1) return 0;
-       for(int i = 2; i < n;i++){
-        if(seive[i] == true){
-            cnt++;
-            for(int j = i; j<n;j+=i){
-                seive[j] = false;
-
+        int cnt = 0;
+        // using seivem algo for prime counting
+        vector<bool> seive(n, true);
+        if (n == 0 || n == 1)
+            return 0;
+        for (int i = 2; i < n; i++) {
+            if (seive[i] == true) {
+                cnt++;
+                for (int j = i + i; j < n; j += i) {
+                    seive[j] = false;
+                }
             }
         }
-       }
-       return cnt;
+        return cnt;
     }
 };
